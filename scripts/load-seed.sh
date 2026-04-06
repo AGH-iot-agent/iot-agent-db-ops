cf48e15f543397717d795a0b52dcd2391e388c7e
dominiks01 <dominikszot950@gmail.com>
2026-04-06 20:43:53 +0200

    fix: pass fix

--- scripts/load-seed.sh
+++ scripts/load-seed.sh
@@ -21,11 +21,12 @@ DB_USER="gte"
 DB_NAME="iot_agent"
 
 POD_NAME="$(kubectl get pods -n "$NAMESPACE" -l app.kubernetes.io/instance="$RELEASE" -o jsonpath='{.items[0].metadata.name}')"
+POSTGRES_PASSWORD="$(kubectl get secret -n "$NAMESPACE" "${RELEASE}-postgresql" -o jsonpath='{.data.password}' | base64 -d)"
 
 kubectl cp sql/00-schema.sql "$NAMESPACE/$POD_NAME:/tmp/00-schema.sql"
 kubectl cp sql/10-seed-dev.sql "$NAMESPACE/$POD_NAME:/tmp/10-seed-dev.sql"
 
-kubectl exec -n "$NAMESPACE" "$POD_NAME" -- psql -U "$DB_USER" -d "$DB_NAME" -f /tmp/00-schema.sql
-kubectl exec -n "$NAMESPACE" "$POD_NAME" -- psql -U "$DB_USER" -d "$DB_NAME" -f /tmp/10-seed-dev.sql
+kubectl exec -n "$NAMESPACE" "$POD_NAME" -- env PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$DB_USER" -d "$DB_NAME" -f /tmp/00-schema.sql
+kubectl exec -n "$NAMESPACE" "$POD_NAME" -- env PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$DB_USER" -d "$DB_NAME" -f /tmp/10-seed-dev.sql
 
 echo "Schema and seed loaded into $DB_NAME ($NAMESPACE)"
